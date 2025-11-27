ee1647e0335d72dfc5c063530fdc88a827f581ad
gantarimentari <larasatigantari@gmail.com>
2025-11-27 16:06:25 +0700

    update icons

--- components/layout/BookDetailLayout.jsx
+++ components/layout/BookDetailLayout.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from 'react';
-import { ActionLikeIcon, CloseIcon, LeftArrowIcon, RightArrowIcon  } from '../Icons';
+import { ActionLikeIcon, CloseIcon, LeftArrowIcon, RightArrowIcon, CartIcon, EyeIcon  } from '../Icons';
 import { useWishlist } from '../context/WishlistContext';
 import { useCart } from '../context/CartContext';
 import { extractBookPrice, formatPrice as formatPriceUtil } from '../utils/priceUtils';
@@ -205,19 +205,18 @@ export default function BookDetailLayout({ book = null, onClick }) {
                   filled={isInWishlist}
                 />
               </button>
-              <button 
+              <button
                 onClick={handleAddToCart}
                 className="p-3 bg-pr-0 rounded-full hover:bg-nt-1 transition-colors"
                 aria-label="Add to cart"
               >
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
+                <CartIcon 
+                  className="w-6 h-6"
+                  color='black'
+                />
               </button>
               <button className="p-3 bg-pr-0 rounded-full hover:bg-nt-1 transition-colors">
-                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                </svg>
+                <EyeIcon/>
               </button>
             </div>
           </div>
